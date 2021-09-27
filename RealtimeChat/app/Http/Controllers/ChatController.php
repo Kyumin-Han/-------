eba5170bab0a92729ccd37c1f1d456c8a2c68bdd
kyumin <4rbals30@naver.com>
2021-09-27 17:51:49 +0900

    채팅 사이트 구현 시작

---
+++ RealtimeChat/app/Http/Controllers/ChatController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ChatController extends Controller
+{
+    // 채팅방 리스트 출력
+    // 특정 채팅방의 메세지 리스트 출력
+    // 메세지 저장
+
+    public function rooms() {
+
+    }
+
+    public function messages($roomId) {
+
+    }
+
+    public function newMessage(Request $request, $roomId) {
+
+    }
+}
