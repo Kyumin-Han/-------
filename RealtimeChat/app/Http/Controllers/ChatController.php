d9e0477009421b0be7d25226f65b039b196bd92b
kyumin <4rbals30@naver.com>
2021-10-05 20:32:05 +0900

    Chatting vue 생성

--- RealtimeChat/app/Http/Controllers/ChatController.php
+++ RealtimeChat/app/Http/Controllers/ChatController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ChatMessage;
+use App\Models\ChatRoom;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ChatController extends Controller
 {
@@ -11,14 +14,42 @@ class ChatController extends Controller
     // 메세지 저장
 
     public function rooms() {
-
+        // vue를 이용하기 때문에 blade 파일이 아니라 vue 페이지 자체를 리턴해주기 때문에 데이터만 주면 된다
+        return ChatRoom::all();
     }
 
     public function messages($roomId) {
+        // select * from chat_messages where room_id = ? 문장을 실행해야 한다
+        // return ChatMessage::where('chat_room_id', $roomId)->latest()->get();
+        // find는 기본키를 가지고 데이터를 사용한다
 
+        // lazy loading VS. eager loading
+        // lazy loading -> 조인을 해서 값을 가져올 경우에 조인을 하기 전 데이터만 먼저 보내주는 방식, 정보에 접근 할 때 값을 가져온다.
+        // blade를 이용할때는 상관 없지만 vue의 axios에는 값만 넘겨주기 때문에 user에 접근 할 수 없기 때문에 with절을 이용해서 처음부터 연계된 테이블의 정보까지 같이 넘겨주는 방식을 eager loading 이라고 한다
+
+        $msgs = ChatMessage::where('chat_room_id', $roomId)->with('user')->latest()->get();
+        // with를 통해 관계가 정의된 테이블의 정보도 같이 준다
+        // dd($msgs);
+        $msgs[0]->user->name;
+        return $msgs;
     }
 
     public function newMessage(Request $request, $roomId) {
+        $request->validate(['message' => 'required']);
 
+        $msg = ChatMessage::create([
+            'user_id' => auth()->user()->name,
+            'chat_room_id' => $roomId,
+            'message' => $request->message,
+        ]);
+
+        return $msg;
+    }
+
+    public function index() {
+        // inertia pacade를 이용해서 vue파일을 불러온다
+        // vue파일의 이름을 인자로 넘겨주어 페이지를 호출한다
+        return Inertia::render('Chat/container');
+        // vue.js파일은 resources의 js->pages에서 찾는다
     }
 }
---
+++ RealtimeChat/resources/js/Pages/Chat/messageContainer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p v-for="msg in messages" :key="msg.id">
+            {{ msg.message }}
+        </p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props:['messages'],
+    
+}
+</script>
